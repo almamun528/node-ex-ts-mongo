78c2b679c235fb8de1e542c433f15a1437ce2f74
almamun528 <ayapths@gmail.com>
2025-02-07 02:10:26 +0600

    Lesson 6 external package

---
+++ Lesson6/index.js
@@ -0,0 +1,7 @@
+// ---------Lesson 6------ // Youtube Asisul Islam video 8 
+
+//topic- external module (nodemon)
+// Random fruits name - packages  
+const getRandomFruitsName = require('random-fruits-name')
+console.log(getRandomFruitsName()) //get Different fruits 
+
