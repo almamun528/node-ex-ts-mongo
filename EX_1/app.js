14220f8696c7f0e5bc2ad91ce9b892e543db5465
almamun528 <ayapths@gmail.com>
2025-02-09 18:35:33 +0600

    add express.Router

--- EX_1/app.js
+++ EX_1/app.js
@@ -1,31 +1,18 @@
 const express = require('express') //-----> import express 
 const app = express() // store the express in a variable 
- 
+const userRouter = require('./users.route')
+
+// make extra 2 url key before the route 
+app.use('/api/user/',userRouter)
 
 
 
-// send a response to the server page so that you might see anything
-app.get("/", (req, res) => {
-    // routing default route for home page..
-  res.send("server is running in your port for HOME route ");
-});
-// post
-app.post("/post", (req, res) => {
+
+app.use("/", (req, res) => {
   // routing default route for home page..
-  res.send("server is running in your port for POST route ");
-  
+  res.send(" <h1> server is running in your port for HOME route  </h1> ");
+  res.end()
 });
-// Put
-app.put("/put", (req, res) => {
-  // routing default route for home page..
-  res.send("server is running in your port for PUT route ");
-});
-// delete 
-app.delete("/delete", (req, res) => {
-  // routing default route for home page..
-  res.send("server is running in your port for DELETE route ");
-});
-
 
 
 // export the express which is stored into app 
---
+++ EX_1/users.route.js
@@ -0,0 +1,19 @@
+// user router 
+const express = require('express')
+const router = express.Router()
+
+
+
+// login router
+router.get("/login", (req, res) => {
+  res.send("login router is founded");
+  res.end();
+});
+// Register router
+router.get("/register", (req, res) => {
+  res.send("register your account now");
+  res.end();
+});
+
+
+module.exports = router
