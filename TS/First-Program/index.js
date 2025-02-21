434f8077372951bb34a3383048bf7726f9d044c5
almamun528 <ayapths@gmail.com>
2025-02-21 23:47:11 +0600

    start ts

---
+++ TS/First-Program/index.js
@@ -0,0 +1,15 @@
+// tutorial 3 : First Typescript program
+var myProgram = 'hello typescript world ';
+// console.log(myProgram)
+var userName = "Al Mamun";
+userName = 'Abdullah Al Mamun';
+// console.log(userName)
+var age;
+age = 23;
+console.log(age);
+function addNumber(num1, num2) {
+    console.log('result is = ', num1 + num2);
+}
+addNumber(20, 22);
+addNumber(20, 1);
+//index.ts ----> tsc index.ts 
---
+++ TS/First-Program/index.ts
@@ -0,0 +1,18 @@
+// tutorial 3 : First Typescript program
+let myProgram = 'hello typescript world '
+// console.log(myProgram)
+let userName = "Al Mamun"
+userName='Abdullah Al Mamun'
+// console.log(userName)
+let age:number
+ age = 23
+console.log(age);
+
+function addNumber(num1:number,num2:number){
+console.log('result is = ',num1+num2)
+}
+
+addNumber(20,22)
+addNumber(20,1)
+
+//index.ts ----> tsc index.ts 
