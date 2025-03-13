4303f89a5afda1e863c7dd7cd6c9a8264810269d
almamun528 <ayapths@gmail.com>
2025-03-13 23:45:37 +0600

    array object

--- TS/First-Program/index.ts
+++ TS/First-Program/index.ts
@@ -1,18 +1,51 @@
-// tutorial 3 : First Typescript program
-let myProgram = 'hello typescript world '
-// console.log(myProgram)
-let userName = "Al Mamun"
-userName='Abdullah Al Mamun'
-// console.log(userName)
-let age:number
- age = 23
-console.log(age);
+// define all the type first 
+let fullName:string='Abdullah Al Mamun';
+let age:number=25;
+let isJunior : boolean= true;
+let myVariable: any= 'hello' //any data can be store into this variable 
+let variableOfInput:string | number = 30; //union data type either data can be string or number 
 
-function addNumber(num1:number,num2:number){
-console.log('result is = ',num1+num2)
-}
+// Basic type is done
 
-addNumber(20,22)
-addNumber(20,1)
+// function
+const ageCal = (name: string, age: number):string => {
+  return `hi $(name) you are $(age)`;
+};
+console.log(ageCal("hello", 30))
 
-//index.ts ----> tsc index.ts 
+const moneyCal =():void=>{console.log(2+2)}
+
+const monCal =(): never =>{throw new Error(' error happens')}
+
+// array 
+const food: string[] = ["apple", "mango", "orang"];
+// food array will return string value 
+const animal: (string | boolean)[] = ['cow', 'cat', 'fox'] // use union option into this array 
+const information: [string, number, boolean] = ["Al Mamun", 30, true];
+// in this here input must needs to serial wise input
+//if you push anything in this array automatic allows to any input and keep  at the end 
+
+// Object is looks like this 
+const student: { name: string; age: number; skill: boolean } = {
+  name: "Pori mini",
+  age: 30,
+  skill: true,
+};
+// define the type of the key then 
+const teacher: { readonly name: string; age: number; skill?: boolean } = {
+  name: "Pori mini",
+//   we can't change the name key because it is read only
+  age: 30,
+// if we use ? optional chaining then it allows us not use it.
+};
+// !store all types into a single variable then write the object
+type User = {
+  readonly name: string;
+  age: number;
+  skill?: boolean;
+};
+const stuff: User = {
+  name: "Mamun",
+  age: 30,
+  skill: true,
+};
